56b7a7537ca5ffa107071a206a7b12d6df2e9cea
Sungguk Lim <limasdf@gmail.com>
2014-11-15 19:41:28 +0900

    add inform message

--- bin/csp_fixer.dart
+++ bin/csp_fixer.dart
@@ -4,10 +4,18 @@
 library csp_fixer.cli;
 
 import 'dart:io';
-import 'package:path/path.dart';
 import 'package:csp_fixer/csp_fixer.dart' as csp_fixer;
 
 void main(List<String> args)
 {
-  csp_fixer.Fix((new FileSystemEntity(args[0]));
+  if (args.length == 0) {
+    print ("csp_fixer will remove inline javascript and generate external javascript file.");
+    print ("Usage: csp_fixer <directory || file>");
+    print ("  e.g,.: csp_fixer ./core-meta");
+    exit(-1);
+  }
+
+  String entity = args[0];
+  csp_fixer.Fix(FileSystemEntity.isDirectorySync(entity) ? 
+                new Directory(entity) : new File(entity));
 }
